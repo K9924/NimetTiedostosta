8d6d9e1d698f3a80a60ee1439e86a97f6b0ceba1
Miikka Solonen <K9924@student.jamk.fi>
2017-01-29 22:45:22 +0200

    valmis

--- NimetTiedistista/Program.cs
+++ NimetTiedistista/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace NimetTiedistista
 {
@@ -10,6 +11,44 @@ namespace NimetTiedistista
     {
         static void Main(string[] args)
         {
+            //TArakasta löytyykö tiedosto samasta hakemistosta
+            if (File.Exists("names.txt"))
+            {
+                Console.WriteLine("Tiedosto löytyy samasta hakemistosta.");
+            }
+            //Tarkasta löytyykö tiedosto
+            if (File.Exists(@"D:\Temp\names.exe"))
+            {
+                Console.WriteLine("Tiedosto löytyy D-asemalta ja  Temp -kansiosta.");
+            }
+            // katsotaan löytyykö D:\ asemalta
+            bool exists = File.Exists("C:\\names.txt");
+            Console.WriteLine(exists);
+
+            var names = new List<string>();
+
+            //Haetaan tiedostosta nimet ja tungetaan listaan
+                using (var sr = new StreamReader(@"D:\Temp\names.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) !=null)
+                    {
+                        names.Add(line);
+                    }
+                }
+
+           
+                //Suoritetaan Uhrauksia pimeyden alttarille ja tapahtuu magiaa
+           var nimetx1 = names.GroupBy(x => x)
+                .Select(g => new { Value = g.Key, Count = g.Count() })
+            .OrderByDescending(x => x.Count);
+
+            //JA hih hei loopilla pihalle
+            foreach (var x in nimetx1)
+            {
+                Console.WriteLine("nimi: " + x.Value + " esiintyy: " + x.Count);
+            }
+           
         }
     }
 }
